7c9de72e794d4ef2527f4628a10331bf8327847b
Sergio Ocon <sergio.ocon@redhat.com>
2017-08-06 09:28:53 +0200

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
   root 'welcome#index'
-  resources :articles do
-    resources :comments
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
