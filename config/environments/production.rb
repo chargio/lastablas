31355a172bdd81ec1b219fa077594a4d253a68b8
Sergio Ocon <sergio.ocon@redhat.com>
2017-08-09 11:33:58 +0200

    adding MAIL_URL variable for the emails sent

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   # Config for action mailer:
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = { host: ENV["HOSTNAME"] }
+  config.action_mailer.default_url_options = { host: ENV["MAIL_URL"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
