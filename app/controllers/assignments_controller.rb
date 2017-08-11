48156a071a6e9f387d216592b719fab49ddac103
Sergio Ocón <sergio.ocon@google.com>
2017-08-11 21:06:28 +0200

    adding assignmnent

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,27 @@
 class AssignmentsController < ApplicationController
+  def new
+    unless(@offspring = Offspring.includes(:user).find_by(id: params[:offspring_id], user_id: current_user.id))
+      flash[:alert] = t '.offspring_not_found'
+      redirect_to offsprings_path
+    end
+    @rooms = Room.all.includes(:shifts)
+    @shifts = Shift.all
+  end
+
+  def create
+    @assignment = Assignment.find_by(offspring: params[:offspring_id]) || Assignment.new
+    @assignment.shift = Shift.find_by(id: params[:shift_id])
+    @assignment.offspring = Offspring.find_by(id: params[:offspring_id], user: current_user)
+
+    if @assignment.offspring.nil?
+      flash[:alert] = (t '.assignment_not_added')
+      redirect_to offsprings_path
+    elsif @assignment.save
+      flash[:success] = (t '.assignment_added')
+      redirect_to offsprings_path
+    else
+      flash[:alert] = (t '.assignment_not_added')
+      render 'new'
+    end
+  end
 end
--- app/models/offspring.rb
+++ app/models/offspring.rb
@@ -1,5 +1,6 @@
 class Offspring < ApplicationRecord
   belongs_to :user
+  has_one :assignment
   enum grade: { primary_first: 1, primary_second: 2, primary_third: 3, others: 0 }
   validates :first_name, presence: true, length: { within: 2..60 }
   validates :last_name,  presence: true, length: { within: 2..60 }
